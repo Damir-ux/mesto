09388ad97e4082037efd496f2abae8ed7b9f9de3
Damir <monte-carlo1991@yandex.ru>
2023-05-10 00:08:58 +0300

    Проектная работа №4 на проверку

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+const profileTitleNode = document.querySelector('.profile__title');
+const profileSubtitleNode = document.querySelector('.profile__subtitle');
+
+let $like = document.querySelector('.photo-grid__button');
+$like.addEventListener( 'click', () => 
+$like.classList.toggle('photo-grid__button_active') )
 
 
 function openPopup() {
-    alert("Вазааап");
     // console.log('openPopup');
     popupElement.classList.add('popup_opened');
 }
+
+function setPopupInputValue(){
+    nameInput.value = profileTitleNode.textContent.trim();
+    jobInput.value = profileSubtitleNode.textContent.trim();
+}
+
+function setNodeTextValue(){
+    profileTitleNode.textContent = nameInput.value;
+    profileSubtitleNode.textContent = jobInput.value;
+}
+
 
 function closePopup() {
 //     console.log('closePopup');
@@ -25,3 +41,19 @@ function closePopup() {
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 // popupElement.addEventListener('click', closePopupByClickOnOverlay)
+
+let formElement = document.querySelector(".popup__button");
+// Находим поля формы в DOM
+let nameInput = document.querySelector(".popup__text_title");
+let jobInput = document.querySelector(".popup__text_subtitle");
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    setNodeTextValue();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('click', handleFormSubmit);
