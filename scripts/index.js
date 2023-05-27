c9ad4c7fac16859462e2131d33a90cae8ffbe53f
Damir <monte-carlo1991@yandex.ru>
2023-05-27 20:35:52 +0300

    Проектная работа №5 на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,90 @@
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = document.querySelector('.popup__close');
-const popupOpenButtonElement = document.querySelector('.profile__button-edit');
-const profileTitleNode = document.querySelector('.profile__title');
-const profileSubtitleNode = document.querySelector('.profile__subtitle');
 
-
-function openPopup() {
-    // console.log('openPopup');
-    popupElement.classList.add('popup_opened');
-    nameInput.value = profileTitleNode.textContent.trim();
-    jobInput.value = profileSubtitleNode.textContent.trim();
-}
-
-function setNodeTextValue(){
-    profileTitleNode.textContent = nameInput.value;
-    profileSubtitleNode.textContent = jobInput.value;
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
 
-function closePopup() {
-//     console.log('closePopup');
-     popupElement.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+
+const profilePopup = document.querySelector('.profile-popup');
+const profilePopupOpenButton = document.querySelector('.profile__button-edit');
+const profilePopupCloseButton = profilePopup.querySelector('.popup__close');
+
+profilePopupOpenButton.addEventListener('click', function () {
+  openPopup(profilePopup);
+});
+
+profilePopupCloseButton.addEventListener('click', function () {
+  closePopup(profilePopup);
+});
 
 
+const addPopup = document.querySelector('.popup-add');
+const addPopupOpenButton = document.querySelector('.profile__button-add');
+const addPopupCloseButton = addPopup.querySelector('.popup__close');
 
-let formElement = document.querySelector(".popup__form");
+addPopupOpenButton.addEventListener('click', function () {
+  openPopup(addPopup);
+});
+
+addPopupCloseButton.addEventListener('click', function () {
+  closePopup(addPopup);
+});
 
 
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input_type_name");
-let jobInput = document.querySelector(".popup__input_type_profession");
+const imagePopup = document.querySelector('.popup-cards');
+const imagePopupCloseButton = imagePopup.querySelector('.popup__close');
+
+imagePopupCloseButton.addEventListener('click', function () {
+  closePopup(imagePopup);
+});
 
 
+function createCardElement(data) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.content.cloneNode(true).querySelector('.photo-grid__item');
+  
+  const imageElement = cardElement.querySelector('.photo-grid__image');
+  imageElement.src = data.link;
+  imageElement.alt = data.name;
 
-function handleFormSubmit (evt) {      
-    evt.preventDefault();
-    setNodeTextValue();
-    closePopup();
+  const textElement = cardElement.querySelector('.photo-grid__text');
+  textElement.textContent = data.name;
+
+  const deleteButton = cardElement.querySelector('.photo-grid__trash');
+  deleteButton.addEventListener('click', function () {
+    cardElement.remove();
+  });
+
+  const viewButton = cardElement.querySelector('.photo-grid__image');
+  viewButton.addEventListener('click', function () {
+    const popupPhoto = imagePopup.querySelector('.popup__photo');
+    const popupCaption = imagePopup.querySelector('.popup__photo-cap');
+
+    popupPhoto.src = data.link;
+    popupPhoto.alt = data.name;
+    popupCaption.textContent = data.name;
+
+    openPopup(imagePopup);
+  });
+
+  const likeButton = cardElement.querySelector('.photo-grid__button');
+  likeButton.addEventListener('click', function () {
+    likeButton.classList.toggle('photo-grid__button_active');
+  });
+
+  return cardElement;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
 
-
-// 1. Загружаем карточки на страницу с помощью Java Script
-
+function addCard(data) {
+  const cardContainer = document.querySelector('.photo-grid');
+  const newCardElement = createCardElement(data);
+  cardContainer.appendChild(newCardElement);
+}
 
 
   const initialCards = [
@@ -72,154 +107,16 @@ formElement.addEventListener('submit', handleFormSubmit);
       {
           name: 'Холмогорский район',
           link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-      }
+      },
+
+      {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
   ];
 
-  const photoGrid = document.querySelector('.photo-grid');
-
-  initialCards.forEach(function(card) {
-      const cardElement = createCardElement(card);
-      photoGrid.appendChild(cardElement);
-  });
-
-  function createCardElement(card) {
-      const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
-      const cardImage = cardTemplate.querySelector('.photo-grid__image');
-      const cardText = cardTemplate.querySelector('.photo-grid__text');
-
-      cardImage.src = card.link;
-      cardImage.alt = card.name;
-      cardText.textContent = card.name;
-
-      return cardTemplate;
-  };
-
-
-// 2. Форма добавления карточки
-
-document.addEventListener('DOMContentLoaded', function() {
-  const photoGrid = document.querySelector('.photo-grid');
-  const addForm = document.forms['add-form'];
-  const titleInput = addForm.elements['title'];
-  const linkInput = addForm.elements['link'];
-
-  addForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const title = titleInput.value;
-    const link = linkInput.value;
-
-    if (title && link) {
-      const newCardHTML = `
-        <div class="photo-grid__item">
-          <button class="photo-grid__trash" type="button"></button>
-          <img src="${link}" alt="${title}" class="photo-grid__image">
-          <div class="photo-grid__signature">
-            <h2 class="photo-grid__text">${title}</h2>
-            <button class="photo-grid__button" type="button"></button>
-          </div>
-        </div>
-      `;
-
-      photoGrid.insertAdjacentHTML('beforeend', newCardHTML);
-
-      titleInput.value = '';
-      linkInput.value = '';
-    }
-  });
+initialCards.forEach(function (data) {
+  addCard(data);
 });
-
-
-
-const popupAddButtonElement = document.querySelector('.profile__button-add');
-const popupCloseAddElement = document.querySelector('.popup__close_add');
-const photoGridTextNode = document.querySelector('.photo-grid__text');
-const photoGridimageNode = document.querySelector('.photo-grid__image');
-const popupAddElement = document.querySelector('.popup-add');
-
-function openAdd() {
-    popupAddElement.classList.add('popup_opened');
-    textInput.value = photoGridTextNode.textContent.trim();
-    imageInput.value = photoGridimageNode.textContent.trim();
-}
-
-function closeAdd() {
-    popupAddElement.classList.remove('popup_opened');
-}
-
-popupAddButtonElement.addEventListener('click', openAdd);
-popupCloseAddElement.addEventListener('click', closeAdd);
-
-
-
-// Находим поля формы в DOM
-let textInput = document.querySelector(".popup__input_title");
-let imageInput = document.querySelector(".popup__input_link");
-
-let formElement2 = document.querySelector(".popup__form_add");
-
-function AddFormSubmit (evt) {      
-    evt.preventDefault();
-    
-    closeAdd();
-}
-
-formElement2.addEventListener('submit', AddFormSubmit);
-
-
-// 3. Лайки
-
-const like = document.querySelectorAll('.photo-grid__button');
-like.forEach(function(el) {
-  el.addEventListener('click', function(evt) {
-      evt.target.classList.toggle('photo-grid__button_active');
-  });
-});
-
-// 4.Удаление карточки
-
-const deleteButton = document.querySelector('.photo-grid');
-
-deleteButton.addEventListener('click', function(event) {
-
-  if(event.target.classList.contains('photo-grid__trash'))
-  {
-  const listItem = event.target.closest('.photo-grid__item');
-     if(listItem){
-        listItem.remove();
-      }
-  }
-});
-
-// 5.Открытие попапа с картинкой
-
-
-  const photoGridItems = document.querySelectorAll('.photo-grid__item');
-  const popupCards = document.querySelector('.popup-cards');
-  const popupPhoto = document.querySelector('.popup__photo');
-  const popupPhotoCap = document.querySelector('.popup__photo-cap');
-  const popupCloseCards = document.querySelector('.popup__close_cards');
-
-  photoGridItems.forEach(item => {
-    item.addEventListener('click', function() {
-      const imageSrc = this.querySelector('.photo-grid__image').getAttribute('src');
-      const imageAlt = this.querySelector('.photo-grid__text').textContent;
-
-      popupPhoto.setAttribute('src', imageSrc);
-      popupPhoto.setAttribute('alt', imageAlt);
-      popupPhotoCap.textContent = imageAlt;
-      popupCards.classList.add('popup_opened');
-    });
-  });
-
-  popupCloseCards.addEventListener('click', function() {
-    popupCards.classList.remove('popup_opened');
-  });
-
-
-
-
-
-
 
 
